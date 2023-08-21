e5c5d29bc1f71ff88365272b91f817a8b4cb8493
SpencerSedano <spencer.sv20@gmail.com>
2023-08-21 20:48:32 +0800

    factorial

---
+++ recursion.py
@@ -0,0 +1,19 @@
+#Recursion is when a function calls itself
+
+#Factorial 
+
+# n! = n * (n-1) 
+# if n! is 0, return 1
+
+def factorial(n):
+    if n >= 1:
+        return n * factorial(n - 1)
+    else:
+        return 1
+
+
+print(f"This is f(0): {factorial(0)}")
+print(f"This is f(1): {factorial(1)}")
+print(f"This is f(2): {factorial(2)}")
+print(f"This is f(3): {factorial(3)}")
+print(f"This is f(4): {factorial(4)}")
